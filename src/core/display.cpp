9b3e77d4f0f6b7e18acbee02a91d8d5f636f1791
duynguyendza-cmyk <duynguyendza@gmail.com>
2026-07-27 06:25:42 +0000

    okee

--- src/core/display.cpp
+++ src/core/display.cpp
@@ -613,11 +613,12 @@ int loopOptions(
         if (menuType == MENU_TYPE_MAIN && checkShortcutPress()) break;
 #endif
 
-       /*if (menuType == MENU_TYPE_REGULAR) {
-        //   String txt = options[index].label;
-        displayScrollingText(fileList[index].filename, coord, false);
+       if (menuType == MENU_TYPE_REGULAR) {
+                    String txt = options[index].label;
+                                displayScrollingText(txt, coord, true);
+                                        }
 
-                               }*/
+
 
 
 // Checks ESC Press first, to not exit after PrevPress is processed
@@ -1050,7 +1051,7 @@ Opt_Coord listFiles(int index, std::vector<FileList> fileList) {
     String txt = ">";
     while (i < arraySize) {
         if (i >= start) {
-          //  tft.setCursor(8, 10 + ((i - start) * 10));
+          tft.setCursor(8, 10 + ((i - start) * 10));
             int lineY = 10 + ((i - start) * 10);
             tft.setCursor(8, lineY);
             if (fileList[i].folder == true)
@@ -1058,37 +1059,21 @@ Opt_Coord listFiles(int index, std::vector<FileList> fileList) {
             else if (fileList[i].operation == true) tft.setTextColor(ALCOLOR, bruceConfig.bgColor);
             else { tft.setTextColor(bruceConfig.priColor, bruceConfig.bgColor); }
 
-           /* if (index == i) {
+            if (index == i) {
                 txt = ">";
-                coord.x = 10 + FM * LW;
-                coord.y = tft.getCursorY();
-                coord.size = nchars;
+                coord.x = 8 + FM * LW;
+                coord.y = lineY + 4;
+                coord.size = nchars -1;
                 coord.fgcolor =
                     fileList[i].folder ? getColorVariation(bruceConfig.priColor) : bruceConfig.priColor;
                 coord.bgcolor = bruceConfig.bgColor;
-            }*/
-           if (index == i) {
-                txt = ">";
+            }
 
-                    coord.x = 8 + FM * LW;   // ngay sau dấu >
-                        coord.y = lineY;         // đúng vị trí dòng selected
-                            coord.size = nchars - 1; // trừ ký tự >
-
-                                coord.fgcolor =
-                                        fileList[i].folder
-                                                    ? getColorVariation(bruceConfig.priColor)
-                                                                : bruceConfig.priColor;
-
-                                                                    coord.bgcolor = bruceConfig.bgColor;
-                                                                    }
-           }
             else txt = " ";
+        }
             txt += fileList[i].filename + "                 ";
             tft.println(txt.substring(0, nchars));
             if (index == i) displayScrollingText(fileList[i].filename, coord, false);
-    }
-        i++;
-        if (i == (start + MAX_ITEMS) || i == arraySize) break;
     }
     return coord;
 }
