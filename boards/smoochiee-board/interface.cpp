a2ae57e6f65b4d39757ad37583ea1e61be5b0a71
duynguyendza-cmyk <duynguyendza@gmail.com>
2026-07-27 10:48:24 +0000

    ok

--- boards/smoochiee-board/interface.cpp
+++ boards/smoochiee-board/interface.cpp
@@ -118,19 +118,8 @@ if (dw) {
 ** Turns off the device (or try to)
 **********************************************************************/
 void powerOff() {
-        // Chờ nhả nút
-    while (digitalRead(UP_BTN) == BTN_ACT) {
-     delay(10);
-                        }
-
-                            delay(100);
-
-                                // Đánh thức bằng GPIO0 khi kéo xuống LOW
-                                    esp_sleep_enable_ext0_wakeup(GPIO_NUM_0, 0);
-
-                                        tft.writecommand(0x10);
-                                            esp_deep_sleep_start();
-                                            }
+        return;
+        }
 
 /*********************************************************************
 ** Function: checkReboot
